56155e66dd98dfa68f14080b845bcce1364a3926
Bhavya Jain <64700692+Bhavyajain21@users.noreply.github.com>
2020-10-15 16:37:43 +0530

    Update app.js

--- DRUM/app.js
+++ DRUM/app.js
@@ -7,9 +7,8 @@ window.addEventListener("keydown", function(e) {
     audio.currentTime = 0; //rewind to the start
     audio.play();
     key.classList.add("playing");
-
-
 });
+
 
 function removeTransition(e) {
     // console.log(e);
@@ -19,4 +18,14 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(function(e) {
     // console.log(e);
     e.addEventListener("transitionend", removeTransition);
+    e.addEventListener("click", function() {
+        console.log(e.dataset.key);
+        // var k = e.dataset.key;
+        const audio = document.querySelector(`audio[data-key="${e.dataset.key}"]`);
+        console.log(audio);
+        audio.currentTime = 0;
+        audio.play();
+
+    });
+
 });
